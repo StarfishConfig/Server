5d5bb4caef04c81fc09a8551d55d179a0887c4c3
damon <zhaorong@outlook.com>
2025-10-23 11:41:02 +0800

    Update OpenAPI & Add ASP.NET Core App Structure
    Updated Microsoft.AspNetCore.OpenApi to version 9.0.10.
    Added new project files including Starfish.Application,
    Starfish.Domain, and Starfish.Host targeting .NET 9.0.
    Introduced common.props for shared properties.
    Configured logging and launch settings with new
    appsettings.json files. Set up basic ASP.NET Core
    application with OpenAPI support and HTTP request
    pipeline in Program.cs. Added HomeController for
    basic routing.

---
+++ Source/Starfish.Host/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nerosoft.Starfish.Host.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
