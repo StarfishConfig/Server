1b00bf77a30c89c0215215419122b109780aa2ef
damon <zhaorong@outlook.com>
2025-10-25 00:07:43 +0800

    Add HostServiceModule for application initialization and service configuration

---
+++ Source/Starfish.Host/HostServiceModule.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.FeatureManagement;
+using Nerosoft.Euonia.Hosting;
+using Nerosoft.Euonia.Modularity;
+using Serilog;
+
+namespace Nerosoft.Starfish.Host;
+
+/// <summary>
+/// The host service module for the Starfish application.
+/// </summary>
+/// <remarks>
+/// This module serves as the entry point for the Starfish host application,
+/// configuring and initializing necessary services and components.
+/// </remarks>
+internal class HostServiceModule : ModuleContextBase
+{
+    /// <summary>
+    /// Configures services for the Starfish host application.
+    /// </summary>
+    /// <param name="context"></param>
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddLogging(builder =>
+        {
+            builder.AddConfiguration(Configuration.GetSection("Logging"))
+                   .AddConsole()
+                   .AddDebug()
+                   .AddSerilog();
+        });
+        context.Services.AddHealthChecks();
+        context.Services.AddControllers();
+        context.Services.AddAuthentication(Configuration);
+        context.Services.AddSwagger();
+        context.Services.AddFeatureManagement();
+    }
+
+    /// <inheritdoc />
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        app.ServerFeatures.Get<IServerAddressesFeature>();
+
+        app.UseSerilogRequestLogging();
+        app.UseForwardedHeaders();
+        app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); //.AllowCredentials());
+    }
+}
--- Source/Starfish.Host/Program.cs
+++ Source/Starfish.Host/Program.cs
@@ -1,4 +1,8 @@
+using Nerosoft.Starfish.Host;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddModularityApplication<HostServiceModule>(builder.Configuration);
 
 // Add services to the container.
 
@@ -8,16 +12,39 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.Lifetime.ApplicationStarted.Register(() =>
 {
-    app.MapOpenApi();
-}
+    app.Services.GetRequiredService<ILoggerFactory>().CreateLogger<Program>().LogInformation("Application started");
+    // Custom logic to execute when the application has started
+});
+
+app.Lifetime.ApplicationStopped.Register(() =>
+{
+    app.Services.GetRequiredService<ILoggerFactory>().CreateLogger<Program>().LogInformation("Application stopped");
+});
+
+// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseWebSockets(new WebSocketOptions()
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2),
+    AllowedOrigins = { "*" }
+});
+
+app.InitializeApplication();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("health");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    //endpoints.MapGrpcReflectionService();
+}
 
 app.Run();
