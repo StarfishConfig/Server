b0ab95f1991eef3faf1ba66321dd6d01930cbe04
damon <zhaorong@outlook.com>
2025-10-28 14:30:42 +0800

    Add Starfish.Repository project and update dependencies.

---
+++ Source/Starfish.Repository/RepositoryModule.cs
@@ -0,0 +1,15 @@
+﻿using Nerosoft.Euonia.Modularity;
+
+namespace Nerosoft.Starfish.Repository;
+
+/// <summary>
+/// Defines the repository module for the Starfish application.
+/// </summary>
+public class RepositoryModule : ModuleContextBase
+{
+    /// <inheritdoc />
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+    }
+}
