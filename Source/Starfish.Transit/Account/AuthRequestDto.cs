01bd537b3896dabcb0f9e645141d8ad09f0315e8
damon <zhaorong@outlook.com>
2025-11-10 13:23:17 +0800

    Update package version and add Auth DTO classes.

---
+++ Source/Starfish.Transit/Account/AuthRequestDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Nerosoft.Starfish.Transit;
+
+/// <summary>
+/// Defines the user authentication request data transfer object.
+/// </summary>
+public class AuthRequestDto
+{
+    /// <summary>
+    /// Gets or sets the username.
+    /// </summary>
+    public string Username { get; set; }
+
+    /// <summary>
+    /// Gets or sets the password.
+    /// </summary>
+    public string Password { get; set; }
+
+    /// <summary>
+    /// Gets or sets the grant provider.
+    /// </summary>
+    /// <value>Username/Email/Phone/Github/Microsoft/Google etc.</value>
+    public string Provider { get; set; }
+
+    /// <summary>
+    /// Gets or sets the request ID for the authentication request.
+    /// </summary>
+    public string RequestId { get; set; }
+}
---
+++ Source/Starfish.Transit/Account/AuthResultDto.cs
@@ -0,0 +1,42 @@
+﻿namespace Nerosoft.Starfish.Transit;
+
+/// <summary>
+/// Represents the data transfer object for authentication response.
+/// </summary>
+public class AuthResultDto
+{
+    /// <summary>
+    /// Gets or sets the access token.
+    /// </summary>
+    public string AccessToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the refresh token.
+    /// </summary>
+    public string RefreshToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the token type.
+    /// </summary>
+    public string TokenType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the expires time in seconds.
+    /// </summary>
+    public long ExpiresIn { get; set; }
+
+    /// <summary>
+    /// Gets or sets the token issues time.
+    /// </summary>
+    public long IssueAt { get; set; }
+
+    /// <summary>
+    /// Gets or sets the user unique identifier.
+    /// </summary>
+    public long UserId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the username.
+    /// </summary>
+    public string Username { get; set; }
+}
