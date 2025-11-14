653ef34ea225decf9908c49b7d1faf6681d099a0
damon <zhaorong@outlook.com>
2025-11-14 23:27:33 +0800

    Enhance data seeding functionality with dynamic configuration and improved error handling

--- Source/Starfish.Repository/Seedwork/DataSeeder.cs
+++ Source/Starfish.Repository/Seedwork/DataSeeder.cs
@@ -11,82 +11,87 @@ namespace Nerosoft.Starfish.Repository;
 /// </summary>
 /// <param name="configuration"></param>
 internal class DataSeeder(IConfiguration configuration, IServiceProvider provider, ILoggerFactory logger)
-    : BackgroundService
+	: BackgroundService
 {
-    private readonly ILogger<DataSeeder> _logger = logger.CreateLogger<DataSeeder>();
+	private readonly ILogger<DataSeeder> _logger = logger.CreateLogger<DataSeeder>();
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        try
-        {
-            var tasks = new List<Task>
-            {
-                SeedingAccountDataAsync(stoppingToken),
-                SeedingDictionaryDataAsync(stoppingToken)
-            };
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		try
+		{
+			var tasks = new List<Task>
+			{
+				SeedingAccountDataAsync(stoppingToken),
+				SeedingDictionaryDataAsync(stoppingToken)
+			};
 
-            await Task.WhenAll(tasks);
-        }
-        catch (Exception exception)
-        {
-            _logger.LogError(exception, "An exception occurred");
-        }
-    }
+			await Task.WhenAll(tasks);
+		}
+		catch (Exception exception)
+		{
+			_logger.LogError(exception, "An exception occurred");
+		}
+	}
 
-    /// <summary>
-    /// Seeds initial account data into the database.
-    /// </summary>
-    /// <param name="cancellationToken"></param>
-    public async Task SeedingAccountDataAsync(CancellationToken cancellationToken = default)
-    {
-        var context = provider.CreateScope().ServiceProvider.GetService<IdentityDataContext>();
+	/// <summary>
+	/// Seeds initial account data into the database.
+	/// </summary>
+	/// <param name="cancellationToken"></param>
+	private async Task SeedingAccountDataAsync(CancellationToken cancellationToken = default)
+	{
+		var username = configuration.GetValue<string>("DataSeeding:Users:0:Username");
+		var password = configuration.GetValue<string>("DataSeeding:Users:0:Password");
 
-        var username = "admin";
-        var password = "nerosoft.8888";
+		if (string.IsNullOrWhiteSpace(username))
+		{
+			return;
+		}
 
-        var exists = await context.Set<User>().AnyAsync(u => u.Username == username, cancellationToken);
-        if (exists)
-        {
-            return;
-        }
+		var context = provider.CreateScope().ServiceProvider.GetService<IdentityDataContext>();
 
-        var user = User.Create(username, UserCreationSource.InitialImport);
-        user.SetPassword(password);
-        user.SetRoles("SA");
+		var exists = await context.Set<User>().AnyAsync(u => u.Username == username, cancellationToken);
+		if (exists)
+		{
+			return;
+		}
 
-        await context.Set<User>().AddAsync(user, cancellationToken);
-        await context.SaveChangesAsync(true, cancellationToken);
-    }
+		var user = User.Create(username, UserCreationSource.InitialImport);
+		user.SetPassword(password);
+		user.SetRoles("SA");
 
-    private async Task SeedingDictionaryDataAsync(CancellationToken cancellationToken = default)
-    {
-        var context = provider.CreateScope().ServiceProvider.GetService<SystemDataContext>();
+		await context.Set<User>().AddAsync(user, cancellationToken);
+		await context.SaveChangesAsync(true, cancellationToken);
+	}
 
-        var exists = await context.Set<DictionaryRoot>().AnyAsync(cancellationToken);
-        if (exists)
-        {
-            return;
-        }
+	private async Task SeedingDictionaryDataAsync(CancellationToken cancellationToken = default)
+	{
+		var context = provider.CreateScope().ServiceProvider.GetService<SystemDataContext>();
 
-        var roles = DictionaryRoot.Create("role", "System user roles");
-        roles.SetItem(new Dictionary<string, string>
-        {
-            { "SU", "Super User" },
-            { "SA", "System Administrator" },
-            { "HD", "Helpdesk Operator" },
-            { "US", "Normal User" }
-        });
+		var exists = await context.Set<DictionaryRoot>().AnyAsync(cancellationToken);
+		if (exists)
+		{
+			return;
+		}
 
-        var userSources = DictionaryRoot.Create("user_source", "User creation sources");
-        userSources.SetItem(new Dictionary<string, string>
-        {
-            { $"{UserCreationSource.InitialImport}", "Initial data import" },
-            { $"{UserCreationSource.AdminCreated}", "Created by administrator" },
-            { $"{UserCreationSource.SelfRegistered}", "Self registration" }
-        });
+		var roles = DictionaryRoot.Create("role", "System user roles");
+		roles.SetItem(new Dictionary<string, string>
+		{
+			{ "SU", "Super User" },
+			{ "SA", "System Administrator" },
+			{ "HD", "Helpdesk Operator" },
+			{ "US", "Normal User" }
+		});
+
+		var userSources = DictionaryRoot.Create("user_source", "User creation sources");
+		userSources.SetItem(new Dictionary<string, string>
+		{
+			{ $"{UserCreationSource.InitialImport}", "Initial data import" },
+			{ $"{UserCreationSource.AdminCreated}", "Created by administrator" },
+			{ $"{UserCreationSource.SelfRegistered}", "Self registration" }
+		});
 
 
-        await context.Set<DictionaryRoot>().AddRangeAsync([roles, userSources], cancellationToken);
-        await context.SaveChangesAsync(true, cancellationToken);
-    }
+		await context.Set<DictionaryRoot>().AddRangeAsync([roles, userSources], cancellationToken);
+		await context.SaveChangesAsync(true, cancellationToken);
+	}
 }
